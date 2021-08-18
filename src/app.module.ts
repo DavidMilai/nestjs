24612a4766f93efa07a5991e07ed6b1317564a17
DavidMilai <davidmilai67@gmail.com>
2021-08-18 20:21:51 +0300

    connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    TasksModule,
+  ],
 })
 export class AppModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { V4 as UUID } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -21,7 +21,12 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with id "${id}" not found`);
+    }
+    return found;
   }
 
   getAllTasks(): Task[] {
